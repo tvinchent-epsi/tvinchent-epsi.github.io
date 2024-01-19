ba8def26c89b038d5a5ee4a405706c0610ba6a97
tvinchent-epsi <thibault.vinchent@campus-cd.com>
2024-01-19 14:50:07 +0100

    fix js bcz github.io do not use php

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-echo $_GET['cookie'];
+<script>
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const cookieValue = urlParams.get('cookie');
+    console.log(`La valeur du cookie pwd est ${cookieValue}`);
 
-?>
+</script>
 
-<p>J'ai récupéré les informations de votre cookie sur mon site, je peux en faire ce que je veux (me l'envoyer par mail, le stocker dans une base etc..)</p>
+<p>J'ai récupéré les informations de votre cookie (sur le site A) sur mon site perso, je peux en faire ce que je veux (me l'envoyer par mail, le stocker dans une base etc..)</p>
+    
+</body>
+</html>

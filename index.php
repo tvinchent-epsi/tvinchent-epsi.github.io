3cbb3c962fc59f13665c0462a9a258af25f5607b
tvinchent-epsi <thibault.vinchent@campus-cd.com>
2024-01-19 14:36:52 +0100

    feat page d'illustration de faille XSS

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo $_GET['cookie'];
+
+?>
+
+<p>J'ai récupéré les informations de votre cookie sur mon site, je peux en faire ce que je veux (me l'envoyer par mail, le stocker dans une base etc..)</p>
